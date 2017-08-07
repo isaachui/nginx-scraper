e3132fa6b18ed911b09d61146d1e08f47f2ea8e3
Isaac Hui <isaachui89@gmail.com>
2017-08-07 14:00:40 -0700

    add testing for parsenginx

---
+++ parsenginx/parser_test.go
@@ -0,0 +1,82 @@
+package parsenginx
+
+import (
+  "testing"
+
+)
+
+func testDefaultParser(t *testing.T) {
+
+  t.Log("testing default parser")
+
+  np := NewDefaultParser()
+
+  got := np.logFormat
+  expected := `$remote_addr - $http_x_forwarded_for - $http_x_realip - [$time_local]  $scheme $http_x_forwarded_proto $x_forwarded_proto_or_scheme "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent"`
+
+  if got != expected {
+    t.Errorf("expected %v, but got %v", got, expected)
+  }
+  got = np.reference[0]
+  expected = "$remote_addr"
+
+  if got != expected {
+    t.Errorf("expected %v, but got %v", got, expected)
+  }
+
+  got = np.reference[3]
+  expected = " - "
+
+  if got != expected {
+    t.Errorf("expected %v, but got %v", got, expected)
+  }
+
+}
+
+func testCustomParser(t *testing.T) {
+
+  t.Log("testing NewNginxParser")
+
+  customNginxLogFormat := `$remote_addr - "$request" - $status`
+  np := NewNginxParser(customNginxLogFormat)
+
+  got := np.reference[4]
+  expected := "$status"
+
+  if got != expected {
+    t.Errorf("expected %v, but got %v", got, expected)
+  }
+
+  got = np.reference[3]
+  expected = " - "
+
+  if got != expected {
+    t.Errorf("expected %v, but got %v", got, expected)
+  }
+
+}
+
+func testParseLineStatus(t *testing.T) {
+
+  np := NewDefaultParser()
+
+  sampleLogLine := `50.112.166.232 - 50.112.166.232, 192.33.28.238, 50.112.166.232,127.0.0.1 - - - [02/Aug/2015:16:04:19 +0000]  http https,http https,http "GET /api/v1/user HTTP/1.1" 200 3350 "https://release.dollarshaveclub.com/our-products" "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/39.0"`
+
+  got, err := np.ParseLine(sampleLogLine, "$request")
+  expected := "GET /api/v1/user HTTP/1.1"
+  if err != nil {
+    t.Error(err)
+  }
+
+  if got != expected {
+    t.Errorf("expected %v, but got %v", got, expected)
+  }
+
+
+
+}
+
+func testParseLineRequest(t *testing.T) {
+
+
+}
